7a326a3e67fcd5229ca46e541f868105e8464ae6
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-02 16:12:19 -0500

    Initial Browser app with App.js with fetch and setting state with console.log

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url = "https://yogapi.herokuapp.com";
+
+class Price extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      asanas =  []
+    };
+  }
+  
+  componentDidMount() {
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        this.setState {(
+          asanas: data.map(item => ({
+            id:  item.id,
+            sanskrit_name: item.sanskrit_name,
+            english_name: item.english_name,
+            img_url: item.img_url
+          }))
+        )}
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Asana Browser</h1>
+        {/* <Asana key={item.id} item={item.sanskrit_name}/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
