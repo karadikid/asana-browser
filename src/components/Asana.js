0f86ede0302a82a09a7cedc288ce8bc382a18c5f
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-04 18:13:26 -0500

    Added style through Asana.css

--- src/components/Asana.js
+++ src/components/Asana.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-
+import './Asana.css';
 
 class Asana extends Component {
 
   render() {
     console.log(this.props)
     return (
-    <div>
+    <div class="Card">
     <li>{this.props.sanskrit_name}</li>
+    <li>{this.props.english_name}</li>
     <img src={this.props.url}/>
     </div>
     );
