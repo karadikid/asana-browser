3afe4895ae3ad5de8e2b72c332331052d64a2c49
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-05 06:49:17 -0500

    import Card from 'react-boostrap/Card' SUCCESS.  Plus <Card> and <Card.Property> tagging now working

--- src/components/Asana.js
+++ src/components/Asana.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import './Asana.css';
+import Card from "react-bootstrap/Card";
 
 class Asana extends Component {
 
   render() {
     console.log(this.props)
     return (
-    <div class="Card">
-    <li>{this.props.sanskrit_name}</li>
-    <li>{this.props.english_name}</li>
-    <img src={this.props.url}/>
-    </div>
+      <>
+    <Card>
+      <Card.Img src={this.props.url}/>
+        <Card.Body>
+          <Card.Title>{this.props.sanskrit_name}</Card.Title>
+          <Card.Text>{this.props.english_name}</Card.Text>
+        </Card.Body>
+    </Card>
+    </>
     );
   }
 }
-
 export default Asana;
